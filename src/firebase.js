69b5da099b75e6695a58496787d7709871a9f031
IbrajKhan <ibraj.grd@gmail.com>
2021-05-29 22:22:37 +0530

    Install Firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDLzss9FU6-71R316CvMXaFPtCNOegYCWc",
+    authDomain: "app-ae3b3.firebaseapp.com",
+    projectId: "app-ae3b3",
+    storageBucket: "app-ae3b3.appspot.com",
+    messagingSenderId: "492719757375",
+    appId: "1:492719757375:web:3ed34fff0aab6bccc5408a",
+    measurementId: "G-5QGGCB546T"
+  };
