41d8108afb097817e51f3899ded145b8ee7bbe2e
IbrajKhan <ibraj.grd@gmail.com>
2021-06-01 22:14:46 +0530

    Done with Header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import Header from './Components/Header'
-
+import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Header.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <p>hello whats up</p>
+    </div>
+  );
+}
+
+export default Home;
