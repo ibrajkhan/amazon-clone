7444e0fa92829a4112af8a859da2a585f39dbdbf
IbrajKhan <ibraj.grd@gmail.com>
2021-06-08 23:06:25 +0530

    Add Subtotal components for displaying total amount in checkout
    also add Subtotla.css for styling it

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <Header />
         <Switch>
-          <Route path="/check">
+          <Route path="/checkout">
             <Checkout />
           </Route>
           <Route path="/">
--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../Checkout.css";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   return (
@@ -10,6 +11,12 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
+        <div className="checkout__title">
+          <h2>Your Shopping Baskets</h2>
+        </div>
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
       </div>
     </div>
   );
