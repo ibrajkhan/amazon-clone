23d640c714a59cd7e524295255f39c86122be8d0
IbrajKhan <ibraj.grd@gmail.com>
2021-06-16 00:55:11 +0530

    style Login page and also uses useState hooks

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Login.css";
-import { Link } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="Login">
       <Link to="/">
@@ -12,6 +15,33 @@ function Login() {
           alt="logo"
         />
       </Link>
+      <div className="login__container">
+        <h1>Sign-In</h1>
+        <form action="">
+          <h5>Email</h5>
+          <input
+            type="email"
+            name="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            name="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="Login__signInButton">Sing In</button>
+        </form>
+        <p>
+          By continuing, you agree to Amazon's Conditions of Use and Privacy
+          Notice.
+        </p>
+        <button className="Login__registerButton">
+          Create Your Amazon Account
+        </button>
+      </div>
     </div>
   );
 }
