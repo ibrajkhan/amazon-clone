a8076ac4bbe0a7d9f5b0ba018ec6e34667cbc205
IbrajKhan <ibraj.grd@gmail.com>
2021-06-20 23:42:17 +0530

    Render Items inside Payment component
    And Style Payment Component

--- src/Components/Payment.jsx
+++ src/Components/Payment.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../Payment.css";
 import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
 
 function Paymemt() {
   const [{ basket, user }, dispatch] = useStateValue();
@@ -8,16 +10,41 @@ function Paymemt() {
   return (
     <div className="payment">
       <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} Items</Link>)
+        </h1>
         <div className="payment__section">
           <div className="payment__title">
             <h3>Dilevery Address</h3>
           </div>
           <div className="payment__address">
             <p>{user?.email}</p>
+            <p>12/A, Ali Nagar</p>
+            <p>Bhandaridih, Giridih</p>
           </div>
         </div>
-        <div className="payment__section"></div>
-        <div className="payment__section"></div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review Items and Delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                ratings={item.ratings}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details"></div>
+        </div>
       </div>
     </div>
   );
